71ddceeb8107d34f292f00f8e92719c7665217f7
Nav Bhatti <bhatti.nav@gmail.com>
2016-10-09 17:57:52 -0700

    implementing scenes, refactoring

---
+++ Game/TestScene.hpp
@@ -0,0 +1,165 @@
+#pragma once
+
+#include <Odin/SceneManager.hpp>
+#include <Odin/TextureManager.hpp>
+
+using odin::GraphicalComponent;
+using odin::PhysicalComponent;
+using odin::InputManager;
+using odin::Entity;
+using odin::EntityId;
+using odin::EntityView;
+using odin::Scene;
+
+class TestScene : public Scene{
+public:
+
+	int _height, _width;
+	float _scale;
+
+	TestScene(int height, int width, float scale)
+		:Scene()
+		, _height(height)
+		, _width(width)
+		, _scale(scale)
+	{}
+
+	unsigned short _bulletCount = 0;
+
+	virtual void setup_scene() {
+
+		auto background = gfxComponents.add(
+			EntityId(0), GraphicalComponent::makeRect(_width / _scale, _height / _scale));
+		background->texture = 4;
+
+		auto pGfx = gfxComponents.add("player", GraphicalComponent::makeRect(1, 2.33));
+		pGfx->texture = 3;
+
+		b2BodyDef playerDef;
+		playerDef.position = { -7, -4 };
+		playerDef.fixedRotation = true;
+		playerDef.type = b2_dynamicBody;
+		playerDef.gravityScale = 2;
+
+		auto pFsx = fsxComponents.add("player", PhysicalComponent::makeRect(1, 2.33, b2world, playerDef));
+
+		listeners.add("player", [&](const InputManager& inmn, EntityId eid) {
+			return player_input(inmn, EntityView(eid, this));
+		});
+
+		addEqTri({ "tri", 0 }, 2, { 5, -3 }, 0, { 1, 0, 0 }, b2_kinematicBody);
+		auto whttri = addEqTri({ "tri", 1 }, 2, { 0, 0 }, 0, { 1, 1, 1 }, b2_dynamicBody);
+		addEqTri({ "tri", 2 }, 2, { 0.5, 2.5 }, 0, { 0, 0, 1 }, b2_dynamicBody);
+		addEqTri({ "tri", 3 }, 2, { 7, -1 }, 0, { 0, 1, 0 }, b2_kinematicBody);
+
+		fsxComponents[{"tri", 0}]->SetAngularVelocity(-1);
+		fsxComponents[{"tri", 3}]->SetAngularVelocity(2);
+
+		b2RevoluteJointDef rjd;
+		b2BodyDef bodyDef;
+		auto ground = b2world.CreateBody(&bodyDef);
+		rjd.Initialize(ground, fsxComponents[{"tri", 1}].pBody, { 0, 0 });
+		b2world.CreateJoint(&rjd);
+
+		//             eid    dimens   pos   rot   color       body_type
+		addRect("rect", { 1, 2 }, { 0, 7 }, 0, { 1, 1, 0 }, b2_dynamicBody);
+		addRect({ "box", 0 }, { 1, 1 }, { -.5, 4 }, 0, { 1, 0, 1 }, b2_dynamicBody);
+		auto pnkcrate = addRect({ "box", 1 }, { 1, 1 }, { -.5, 5 }, 0, { 1, 0, 1 }, b2_dynamicBody);
+		addRect({ "box", 2 }, { 1, 1 }, { -.5, 6 }, 0, { 1, 0, 1 }, b2_dynamicBody);
+		addRect({ "box", 3 }, { 1, 1 }, { -.5, 7 }, 0, { 1, 0, 1 }, b2_dynamicBody);
+		addRect({ "box", 4 }, { 1, 1 }, { -.5, 8 }, 0, { 1, 0, 1 }, b2_dynamicBody);
+		auto blucrate = addRect({ "box", 5 }, { 1, 1 }, { .1f, 90 }, 0, { 0, 1, 1 }, b2_dynamicBody);
+		addRightTri({ "rtri", 0 }, { -2, 2 }, { 0, -3 }, 0, { 0, 1, 1 }, b2_dynamicBody);
+		auto ylwramp = addRightTri({ "rtri", 1 }, { 3, 1 }, { -2, -3 }, 0, { 1, 1, 0 });
+
+		fireBullet({ -170, 5.5f }, { 100, 0 });
+
+		//GLuint nul = load_texture( "null.png", 0 );
+		GLuint nul = odin::load_texture< GLubyte[4] >(0, 1, 1, { 0xFF, 0xFF, 0xFF, 0xFF });
+		GLuint tex = odin::load_texture(1, "Textures/crate.png");
+		GLuint tex2 = odin::load_texture(2, "Textures/crate2.png");
+		GLuint tex3 = odin::load_texture(3, "Textures/pixel_cowboy.png");
+		GLuint tex4 = odin::load_texture(4, "Textures/background.png");
+
+		blucrate.gfxComponent()->texture = 2;
+		pnkcrate.gfxComponent()->texture = 1;
+		ylwramp.gfxComponent()->texture = 1;
+		whttri.gfxComponent()->texture = 1;
+
+		b2BodyDef floorDef;
+		b2EdgeShape floorShape;
+		floorShape.Set({ -10, -8 }, { 10, -8 });
+
+		fsxComponents["floor"] = b2world.CreateBody(&floorDef);
+		fsxComponents["floor"]->CreateFixture(&floorShape, 1)
+			->SetFriction(odin::PhysicalComponent::DEFAULT_FRICTION);
+
+		floorShape.Set({ 10, +8 }, { 10, -8 });
+
+		fsxComponents["wall"] = b2world.CreateBody(&floorDef);
+		fsxComponents["wall"]->CreateFixture(&floorShape, 1)
+			->SetFriction(odin::PhysicalComponent::DEFAULT_FRICTION);
+
+	}
+
+	EntityView fireBullet(Vec2 position, Vec2 velocity)
+	{
+		EntityId eid("bullet", _bulletCount++);
+
+		if (!entities.add(eid, Entity(position, 0)))
+			std::cout << "Entity " << eid << " already exists.\n";
+
+		if (!gfxComponents.add(eid, GraphicalComponent::makeRect(3.f, .1f)))
+			std::cout << "Entity " << eid << " already has a GraphicalComponent.\n";
+
+		b2BodyDef bodyDef;
+		bodyDef.position = position;
+		bodyDef.linearVelocity = velocity;
+		bodyDef.type = b2_dynamicBody;
+		bodyDef.bullet = true;
+
+		if (!fsxComponents.add(eid, PhysicalComponent::makeCircle(.05f, b2world, bodyDef, 0.01)))
+			std::cout << "Entity " << eid << " already has a PhysicalComponent.\n";
+
+		return EntityView(eid, this);
+	}
+
+	void player_input(const InputManager& mngr, EntityView ntt)
+	{
+		b2Body& body = *ntt.fsxComponent()->pBody;
+
+		Vec2 vel = body.GetLinearVelocity();
+		float maxSpeed = 5.5f;
+		float actionLeft = mngr.isKeyDown(SDLK_LEFT) ? 1 : 0;
+		float actionRight = mngr.isKeyDown(SDLK_RIGHT) ? 1 : 0;
+
+		//b2Fixture* pFixt = body.GetFixtureList();
+
+		if (actionLeft == 0 && actionRight == 0)
+		{
+			//pFixt->SetFriction( 2 );
+			vel.x = tween<float>(vel.x, 0, 12 * (1 / 60.0));
+		}
+		else
+		{
+			//pFixt->SetFriction( 0 );
+			vel.x -= actionLeft * (20 + 1) * (1 / 60.0);
+			vel.x += actionRight * (20 + 1) * (1 / 60.0);
+			vel.x = glm::clamp(vel.x, -maxSpeed, +maxSpeed);
+		}
+
+		if (mngr.wasKeyPressed(SDLK_UP))
+			vel.y = 11;
+
+		if (mngr.wasKeyReleased(SDLK_UP) && vel.y > 0)
+			vel.y *= 0.6f;
+
+		if (mngr.gamepads.wasButtonPressed(0, SDL_CONTROLLER_BUTTON_A))
+			vel.y = 11;
+
+		if (mngr.gamepads.wasButtonReleased(0, SDL_CONTROLLER_BUTTON_A) && vel.y > 0)
+			vel.y *= 0.6f;
+
+		body.SetLinearVelocity(vel);
+	}
+};
---
+++ OdinEngine/includes/Odin/SceneManager.hpp
@@ -0,0 +1,306 @@
+#pragma once
+
+#include "Entity.hpp"
+
+namespace odin {
+	class Scene;
+
+	// Lazy entity proxy object.
+	struct EntityView
+	{
+		EntityId eid;
+		Scene*    pScene;
+
+		EntityView(EntityId eid, Scene* game)
+			: eid(eid), pScene(game)
+		{
+		}
+
+		void attach(GraphicalComponent gfx);
+		void attach(PhysicalComponent fsx);
+		void attach(InputListener lstn);
+
+		void detach(ComponentType type);
+
+		GraphicalComponent* gfxComponent();
+		PhysicalComponent* fsxComponent();
+		InputListener* inputListener();
+	};
+
+	class Scene {
+	public:
+		template< typename ValueType >
+		using EntityMap = odin::BinarySearchMap< EntityId, ValueType >;
+
+
+		EntityMap< Entity >             entities;
+
+		EntityMap< GraphicalComponent > gfxComponents;
+
+		b2ThreadPool                    b2thd;
+		b2World                         b2world = { { 0.f, -9.81f }, &b2thd };
+		EntityMap< PhysicalComponent >  fsxComponents;
+
+		InputManager                    inputManager;
+		EntityMap< InputListener >      listeners;
+
+		virtual void setup_scene(){}
+
+		EntityView addRect(EntityId   eid,
+			Vec2       dimen,
+			Vec2       pos = { 0, 0 },
+			float      rot = 0,
+			glm::vec3  color = { 1, 1, 1 },
+			b2BodyType bodyType = b2BodyType::b2_staticBody)
+		{
+			if (!entities.add(eid, Entity(pos, rot)))
+				std::cout << "Entity " << eid << " already exists.\n";
+
+			if (!gfxComponents.add(eid,
+				GraphicalComponent::makeRect(dimen.x, dimen.y, color)))
+				std::cout << "Entity " << eid << " already has a GraphicalComponent.\n";
+
+			b2BodyDef bodyDef;
+			bodyDef.position = pos;
+			bodyDef.angle = rot;
+			bodyDef.type = bodyType;
+
+			if (!fsxComponents.add(eid,
+				PhysicalComponent::makeRect(dimen.x, dimen.y, b2world, bodyDef)))
+				std::cout << "Entity " << eid << " already has a PhysicalComponent.\n";
+			return EntityView(eid, this);
+		}
+
+		EntityView addRightTri(EntityId   eid,
+			Vec2       dimen,
+			Vec2       pos = { 0, 0 },
+			float      rot = 0,
+			glm::vec3  color = { 1, 1, 1 },
+			b2BodyType bodyType = b2BodyType::b2_staticBody)
+		{
+			if (!entities.add(eid, Entity(pos, rot)))
+				std::cout << "Entity " << eid << " already exists.\n";
+
+			if (!gfxComponents.add(eid,
+				GraphicalComponent::makeRightTri(dimen.x, dimen.y, color)))
+				std::cout << "Entity " << eid << " already has a GraphicalComponent.\n";
+
+			b2BodyDef bodyDef;
+			bodyDef.position = pos;
+			bodyDef.angle = rot;
+			bodyDef.type = bodyType;
+
+			if (!fsxComponents.add(eid,
+				PhysicalComponent::makeRightTri(dimen.x, dimen.y, b2world, bodyDef)))
+				std::cout << "Entity " << eid << " already has a PhysicalComponent.\n";
+
+			return EntityView(eid, this);
+
+		}
+
+		EntityView addEqTri(EntityId   eid,
+			float      length,
+			Vec2       pos = { 0, 0 },
+			float      rot = 0,
+			glm::vec3  color = { 1, 1, 1 },
+			b2BodyType bodyType = b2BodyType::b2_staticBody)
+		{
+			if (!entities.add(eid, Entity(pos, rot)))
+				std::cout << "Entity " << eid << " already exists.\n";
+
+			if (!gfxComponents.add(eid, GraphicalComponent::makeEqTri(length, color)))
+				std::cout << "Entity " << eid << " already has a GraphicalComponent.\n";
+
+			b2BodyDef bodyDef;
+			bodyDef.position = pos;
+			bodyDef.angle = rot;
+			bodyDef.type = bodyType;
+
+			if (!fsxComponents.add(eid, PhysicalComponent::makeEqTri(length, b2world, bodyDef)))
+				std::cout << "Entity " << eid << " already has a PhysicalComponent.\n";
+
+			return EntityView(eid, this);
+		}
+
+		void handleInput() {
+			inputManager.pollEvents();
+			for (auto itr = listeners.begin();
+			itr != listeners.end(); ++itr)
+			{
+				trigger(*itr, itr.key());
+			}
+		}
+
+		void update(float tgtFrameTime) {
+			b2world.Step(tgtFrameTime, 50, 50);
+			for (auto itr = fsxComponents.begin();
+			itr != fsxComponents.end(); ++itr)
+			{
+				updateComponent(*itr, itr.key());
+			}
+		}
+
+		void draw(float zoom, float aspect, GLint uMatrix, GLint uColor, GLint uTexture, GLuint program) {
+			for (auto itr = gfxComponents.begin();
+			itr != gfxComponents.end(); ++itr)
+			{
+				drawComponent(*itr, itr.key(), zoom, aspect, uMatrix, uColor, uTexture, program);
+			}
+		}
+
+		void updateComponent(const PhysicalComponent& fsx, EntityId eid)
+		{
+			Entity& entity = entities[eid];
+
+			entity.position = fsx.position();
+
+			if (fsx.pBody->IsBullet())
+			{
+				Vec2 vel = fsx.pBody->GetLinearVelocity();
+
+				float angle = std::atan2(vel.y, vel.x);
+				fsx.pBody->SetTransform(entity.position, angle);
+			}
+
+			entity.rotation = fsx.rotation();
+		}
+
+
+		void drawComponent(const GraphicalComponent& gfx, EntityId eid, float zoom, float aspect, GLint uMatrix, GLint uColor, GLint uTexture, GLuint program)
+		{
+			using namespace glm;
+			Entity& entity = entities[eid];
+
+			mat4 mtx;
+			mtx = scale(mtx, vec3(zoom, zoom * aspect, 1));
+			mtx = translate(mtx, vec3(entity.position.glmvec2, 0));
+			mtx = rotate(mtx, entity.rotation, vec3(0, 0, 1));
+
+			if (gfx.programId == 0)
+			{
+				glUseProgram(program);
+				glUniform(uMatrix, mtx);
+				glUniform(uColor, gfx.color);
+				glUniform(uTexture, gfx.texture);
+			}
+
+			glBindVertexArray(gfx.vertexArray);
+			glDrawArrays(GL_TRIANGLES, 0, gfx.count);
+		}
+
+		void trigger(const InputListener& lstn, EntityId eid)
+		{
+			lstn(inputManager, eid);
+		}
+
+
+		void add(EntityId eid, GraphicalComponent gfx)
+		{
+			gfxComponents.add(eid, std::move(gfx));
+		}
+
+		void add(EntityId eid, PhysicalComponent fsx)
+		{
+			fsxComponents.add(eid, std::move(fsx));
+		}
+
+		void add(EntityId eid, InputListener lstn)
+		{
+			listeners.add(eid, std::move(lstn));
+		}
+
+
+	};
+
+	class SceneManager {
+	public:
+		int currentScene = -1;
+		std::vector<Scene*> scenes;
+
+		~SceneManager() {
+			for (auto itr = scenes.begin();
+			itr != scenes.end(); ++itr)
+			{
+				delete *itr;
+			}
+		}
+
+		void changeScene(int newScene) {
+			if (newScene < scenes.size()) {
+				currentScene = newScene;
+			}
+		}
+
+		void addScene(int idx, Scene* scene) {
+			if (idx >= scenes.size())
+				scenes.push_back(scene);
+			else {
+				scenes[idx] = scene;
+			}
+		}
+
+		void draw(float zoom, float aspect, GLint uMatrix, GLint uColor, GLint uTexture, GLuint program) {
+			if(currentScene >= 0 && currentScene < scenes.size())
+				scenes[currentScene]->draw(zoom, aspect, uMatrix, uColor, uTexture, program);
+		}
+
+		void handleInput() {
+			if (currentScene >= 0 && currentScene < scenes.size())
+				scenes[currentScene]->handleInput();
+		}
+
+		void update(float tgtFrameTime) {
+			if (currentScene >= 0 && currentScene < scenes.size())
+				scenes[currentScene]->update(tgtFrameTime);
+		}
+	};
+
+	void EntityView::attach(GraphicalComponent gfx)
+	{
+		pScene->gfxComponents[eid] = std::move(gfx);
+	}
+
+	void EntityView::attach(PhysicalComponent fsx)
+	{
+		pScene->fsxComponents[eid] = std::move(fsx);
+	}
+
+	void EntityView::attach(InputListener lstn)
+	{
+		pScene->listeners[eid] = std::move(lstn);
+	}
+
+	void EntityView::detach(ComponentType type)
+	{
+		switch (type)
+		{
+		case ComponentType::Graphical:
+			pScene->gfxComponents.remove(eid);
+			break;
+		case ComponentType::Physical:
+			pScene->fsxComponents.remove(eid);
+			break;
+		case ComponentType::Input:
+			pScene->listeners.remove(eid);
+			break;
+		}
+	}
+
+	GraphicalComponent* EntityView::gfxComponent()
+	{
+		auto itr = pScene->gfxComponents.search(eid);
+		return itr ? (GraphicalComponent*)itr : nullptr;
+	}
+
+	PhysicalComponent* EntityView::fsxComponent()
+	{
+		auto itr = pScene->fsxComponents.search(eid);
+		return itr ? (PhysicalComponent*)itr : nullptr;
+	}
+
+	InputListener* EntityView::inputListener()
+	{
+		auto itr = pScene->listeners.search(eid);
+		return itr ? (InputListener*)itr : nullptr;
+	}
+}
