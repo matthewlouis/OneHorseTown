b57491c3d52cb1aa51118b77c1f024210d6357f2
jsquare89 <jarredjardine@hotmail.com>
2016-11-16 14:11:31 -0800

    level platforms, double jump

--- Game/EntityFactory.h
+++ Game/EntityFactory.h
@@ -75,155 +75,172 @@ enum EntityTypes {
 namespace odin
 {
 
-inline namespace factory
-{
-    extern const int PLAYER_TAG;
-
-    // "using namespace odin::factory;" to just use these functions.
-
-	// graphics component:physics component scale 10:1
-	static const int physicsScale = 10;
-
-	// used for specifying dimensions of objects. 
-	static struct Dim {
-		float x; // actual scale for graphics comp
-		float y;
-		Dim getPhysicsDim() { return Dim{ x / physicsScale, y / physicsScale }; } // return physics dim to scale
-	};
-	// object dimensions
-	static Dim playerDim{ 32,32 }; // intially 32x32
-	static Dim horse{44,36}; // initially 44x36
-	static Dim platform{16,16}; // initially 16x16
-
-    template< typename T >
-    void make_player( T* pScene, EntityId eid, Vec2 pos, uint16_t playerNum )
-    {
-        pScene->entities[ eid ];
-        pScene->entities[ { "playes", playerNum } ];
-
-        decltype(auto) ntt = pScene->entities[ eid ];
-        ntt.position = pos;
-        ntt.setBase( typename T::EntityPlayerType { playerNum, pScene } );
-
-        ntt.pDrawable = pScene->newGraphics( GraphicalComponent::makeRect( playerDim.x, playerDim.y ) );
-        ntt.pDrawable->texture = PLAYER;
-
-        decltype(auto) nttarm = pScene->entities[ { "playes", playerNum } ];
-
-		//add arm
-        nttarm.pDrawable = pScene->newGraphics( GraphicalComponent::makeRect(playerDim.x, playerDim.y));
-		nttarm.pDrawable->texture = ARM_TEXTURE;
-		nttarm.pDrawable->visible = false;
-												//idle 10 frame, run 10 frame, jump 3 frame, shoot 3 frame (one armed, one armless)	
-        //get_components< AnimatorComponent >( pScene ).add( eid, { 10, 10, 3, 3, 10, 10, 3, 3 } );
-		
-        ntt.pAnimator = pScene->newAnimator( { 10, 10, 3, 3, 10, 10, 3, 3 } );
-
-		//Set up arm
-															//5 x shoot animation at 4 frames per anim
-        nttarm.pAnimator = pScene->newAnimator( { 4, 4, 4, 4, 4 } );
-		nttarm.pAnimator->play = false;
-		nttarm.pAnimator->loop = false;
-
-        b2BodyDef playerDef;
-        playerDef.position = pos;
-        playerDef.fixedRotation = true;
-        playerDef.type = b2_dynamicBody;
-        playerDef.gravityScale = 2;
-        playerDef.userData = ntt.base();
-
-        auto pRec = PhysicalComponent::makeRect( playerDim.getPhysicsDim().x/2, playerDim.getPhysicsDim().y, pScene->b2world, playerDef, 1.0, PLAYER, PLATFORM | PLAYER | BULLET );
-        ntt.pBody = pRec.pBody;
-        pRec.pBody = nullptr;
-
-		//a,rm
-		b2BodyDef armDef;
-		armDef.position = pos;
-		armDef.type = b2_staticBody;
-
-		//player arm physics component
-		auto paFsx = PhysicalComponent::makeRect(playerDim.getPhysicsDim().x, playerDim.getPhysicsDim().y, pScene->b2world, armDef, 1.0, PLAYER_ARM, 0);
-        nttarm.pBody = paFsx.pBody;
-        paFsx.pBody = nullptr;
-    }
-
-    template< typename T >
-    void make_horse( T* pScene, EntityId eid, Vec2 pos )
-    {
-        decltype(auto) ntt = pScene->entities[ eid ];
-
-
-        ntt.pDrawable = pScene->newGraphics( GraphicalComponent::makeRect( horse.x, horse.y ) );
-        ntt.pDrawable->texture = HORSE_TEXTURE;
-
-        b2BodyDef horseDef;
-        horseDef.position = pos; 
-        horseDef.fixedRotation = true;
-        horseDef.type = b2_dynamicBody;
-        horseDef.gravityScale = 2;
-
-        auto hFsx = PhysicalComponent::makeRect( horse.getPhysicsDim().x, horse.getPhysicsDim().y, pScene->b2world, horseDef, 1.0, HORSE, PLATFORM );
-        ntt.pBody = hFsx.pBody;
-        hFsx.pBody = nullptr;
-    }
-
-	template< typename T >
-	void make_platform(T* pScene, const char(&id)[7], int length, Vec2 offset = { 0, 0 }, Textures text = GROUND1, Anchors anchor = CENTRE)
+	inline namespace factory
 	{
-		Vec2 start;
+		extern const int PLAYER_TAG;
 
-		float x = 0, y = 0;
+		// "using namespace odin::factory;" to just use these functions.
 
-		switch (anchor)
+		// graphics component:physics component scale 10:1
+		static const int physicsScale = 10;
+
+		// used for specifying dimensions of objects. 
+		static struct Dim {
+			float x; // actual scale for graphics comp
+			float y;
+			Dim getPhysicsDim() { return Dim{ x / physicsScale, y / physicsScale }; } // return physics dim to scale
+		};
+		// object dimensions
+		static Dim playerDim{ 32,32 }; // intially 32x32
+		static Dim horse{ 44,36 }; // initially 44x36
+		static Dim platform{ 16,16 }; // initially 16x16
+
+		template< typename T >
+		void make_player(T* pScene, EntityId eid, Vec2 pos, uint16_t playerNum)
 		{
-		case CENTRE:
-			x += length / 2.0;
-			start = { x, 0 };
-			break;
-		default:
-			break;
+			pScene->entities[eid];
+			pScene->entities[{ "playes", playerNum }];
+
+			decltype(auto) ntt = pScene->entities[eid];
+			ntt.position = pos;
+			ntt.setBase(typename T::EntityPlayerType{ playerNum, pScene });
+
+			ntt.pDrawable = pScene->newGraphics(GraphicalComponent::makeRect(playerDim.x, playerDim.y));
+			ntt.pDrawable->texture = PLAYER;
+
+			decltype(auto) nttarm = pScene->entities[{ "playes", playerNum }];
+
+			//add arm
+			nttarm.pDrawable = pScene->newGraphics(GraphicalComponent::makeRect(playerDim.x, playerDim.y));
+			nttarm.pDrawable->texture = ARM_TEXTURE;
+			nttarm.pDrawable->visible = false;
+			//idle 10 frame, run 10 frame, jump 3 frame, shoot 3 frame (one armed, one armless)	
+//get_components< AnimatorComponent >( pScene ).add( eid, { 10, 10, 3, 3, 10, 10, 3, 3 } );
+
+			ntt.pAnimator = pScene->newAnimator({ 10, 10, 3, 3, 10, 10, 3, 3 });
+
+			//Set up arm
+																//5 x shoot animation at 4 frames per anim
+			nttarm.pAnimator = pScene->newAnimator({ 4, 4, 4, 4, 4 });
+			nttarm.pAnimator->play = false;
+			nttarm.pAnimator->loop = false;
+
+			b2BodyDef playerDef;
+			playerDef.position = pos;
+			playerDef.fixedRotation = true;
+			playerDef.type = b2_dynamicBody;
+			playerDef.gravityScale = 2;
+			playerDef.userData = ntt.base();
+
+			// get player rec
+			auto pRec = PhysicalComponent::makeRect(playerDim.getPhysicsDim().x / 2, playerDim.getPhysicsDim().y, pScene->b2world, playerDef, 1.0, PLAYER, PLATFORM | PLAYER | BULLET);
+			
+			// add player foot sensor fixture
+			//shape definition for sensonr fixture
+			b2PolygonShape polygonShape;
+			polygonShape.SetAsBox(1, 2); //a 2x4 rectangle
+			//fixture definition
+			//b2FixtureDef myFixtureDef;
+			//myFixtureDef.shape = &polygonShape;
+			//myFixtureDef.density = 1;
+			//polygonShape.SetAsBox(0.3, 0.3, b2Vec2(0, -2), 0);
+			//myFixtureDef.isSensor = true;
+			//b2Fixture* footSensorFixture = pRec->CreateFixture(&myFixtureDef);
+			//footSensorFixture->SetUserData((void*)3);
+			
+			// assign player phys to entity
+			ntt.pBody = pRec.pBody;
+			pRec.pBody = nullptr;
+
+			//a,rm
+			b2BodyDef armDef;
+			armDef.position = pos;
+			armDef.type = b2_staticBody;
+
+			//player arm physics component
+			auto paFsx = PhysicalComponent::makeRect(playerDim.getPhysicsDim().x, playerDim.getPhysicsDim().y, pScene->b2world, armDef, 1.0, PLAYER_ARM, 0);
+			nttarm.pBody = paFsx.pBody;
+			paFsx.pBody = nullptr;
 		}
 
-		Vec2 pos{ offset.x + platform.x/2,offset.y };
-		x = 1;
-		EntityId eid;
-		uint16 i;
-		for (i = 0; i < length; i++)
+		template< typename T >
+		void make_horse(T* pScene, EntityId eid, Vec2 pos)
 		{
-			eid = { id, i };
-            decltype(auto) ntt = pScene->entities[ eid ];
-
-            ntt.position = pos;
-
-            ntt.pDrawable = pScene->newGraphics(
-                GraphicalComponent::makeRect( platform.x, platform.y, { 1,1,1 } ) );
-
-            ntt.pDrawable->texture = GROUND1;
-			//makeRect(scene, { id, i }, { 1, 1 }, pos, 0, { 1,1,1 }, GROUND1);
-			
+			decltype(auto) ntt = pScene->entities[eid];
 
 
-			pos.x += platform.x;
-			//offset.x += physicsScale; 
+			ntt.pDrawable = pScene->newGraphics(GraphicalComponent::makeRect(horse.x, horse.y));
+			ntt.pDrawable->texture = HORSE_TEXTURE;
 
-			//float colliderPos = length / 2.0;
-			//offset.x += colliderPos;
-			//fset.x += pos.x;
-		    float newPos = offset.x - (platform.x*length / 2.0f) ;
-		    // Define the physical properties of the platform
+			b2BodyDef horseDef;
+			horseDef.position = pos;
+			horseDef.fixedRotation = true;
+			horseDef.type = b2_dynamicBody;
+			horseDef.gravityScale = 2;
+
+			auto hFsx = PhysicalComponent::makeRect(horse.getPhysicsDim().x, horse.getPhysicsDim().y, pScene->b2world, horseDef, 1.0, HORSE, PLATFORM);
+			ntt.pBody = hFsx.pBody;
+			hFsx.pBody = nullptr;
+		}
+
+		template< typename T >
+		void make_platform(T* pScene, const char(&id)[7], int length, Vec2 offset = { 0, 0 }, Textures text = GROUND1, Anchors anchor = CENTRE)
+		{
+			Vec2 start;
+
+			float x = 0, y = 0;
+
+			switch (anchor)
+			{
+			case CENTRE:
+				x += length / 2.0;
+				start = { x, 0 };
+				break;
+			default:
+				break;
+			}
+
+
+			Vec2 pos{ offset.x,offset.y };
+			x = 1;
+			EntityId eid;
+			uint16_t physID = 0;
+			uint16 i;
+			for (i = 0; i < length; i++)
+			{
+				eid = { id, i };
+				decltype(auto) ntt = pScene->entities[eid];
+
+
+				pos.x += platform.x / 2;
+
+				ntt.position = pos;
+				ntt.pDrawable = pScene->newGraphics(
+					GraphicalComponent::makeRect(platform.x, platform.y, { 1,1,1 }));
+
+				ntt.pDrawable->texture = GROUND1;
+				//makeRect(scene, { id, i }, { 1, 1 }, pos, 0, { 1,1,1 }, GROUND1);
+
+				pos.x += platform.x / 2;
+				physID++;
+			}
+
+			// define another entity for the floors physics only
+			eid = { id, physID };
+			decltype(auto) ntt = pScene->entities[eid];
+			ntt.position = { pos.x + (platform.x*length),pos.y };
+
 			b2BodyDef bodyDef;
-		    bodyDef.position = { offset.x/physicsScale + (((platform.x*length)/2)/physicsScale) ,offset.y/physicsScale};
+			bodyDef.position = { offset.x / physicsScale + (platform.getPhysicsDim().x*length) / 2 ,offset.y / physicsScale };
 			bodyDef.angle = 0;
 			bodyDef.type = b2_staticBody;
 
-            auto pb = PhysicalComponent::makeRect( platform.getPhysicsDim().x, platform.getPhysicsDim().y, pScene->b2world, bodyDef, 1.0, PLATFORM, HORSE | PLAYER );
-            ntt.pBody = pb.pBody;
-            pb.pBody = nullptr;
+			auto pb = PhysicalComponent::makeRect(platform.getPhysicsDim().x*length, platform.getPhysicsDim().y, pScene->b2world, bodyDef, 1.0, PLATFORM, HORSE | PLAYER);
+			ntt.pBody = pb.pBody;
+			pb.pBody = nullptr;
 
-		
 		}
 	}
-}
 }
 
 
--- OdinEngine/includes/Box2D/Dynamics/b2Body.h
+++ OdinEngine/includes/Box2D/Dynamics/b2Body.h
@@ -666,6 +666,7 @@ inline bool b2Body::IsBullet() const
 	return (m_flags & e_bulletFlag) == e_bulletFlag;
 }
 
+
 inline void b2Body::SetAwake(bool flag)
 {
 	if (flag)
