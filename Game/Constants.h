08bbd037cb07517b324253fa0d32f0c52df4399b
Andrew Meckling <andrew.t.meckling@gmail.com>
2016-12-13 15:42:15 -0800

    PIXEL_SIZE now set based on screen resoultion when launching the game.

--- Game/Constants.h
+++ Game/Constants.h
@@ -1,20 +1,20 @@
 #pragma once
 
-const int PIXEL_SIZE = 2;
+int PIXEL_SIZE = 2;
 
 // sega genesis resolution 320x224
 const float VIRTUAL_WIDTH = 480;
 const float VIRTUAL_HEIGHT = 288;
 
 // Screen width.
-const int WIDTH = int( VIRTUAL_WIDTH * PIXEL_SIZE );
+int WIDTH = int( VIRTUAL_WIDTH * PIXEL_SIZE );
 
 // Screen height.
-const int HEIGHT = int( VIRTUAL_HEIGHT * PIXEL_SIZE );
+int HEIGHT = int( VIRTUAL_HEIGHT * PIXEL_SIZE );
 
 // OpenGL draw scaling.
-const int LOW_WIDTH = int( WIDTH / PIXEL_SIZE );
-const int LOW_HEIGHT = int( HEIGHT / PIXEL_SIZE );
+int LOW_WIDTH = int( WIDTH / PIXEL_SIZE );
+int LOW_HEIGHT = int( HEIGHT / PIXEL_SIZE );
 
 const float SCALE = VIRTUAL_WIDTH / 2;
 
